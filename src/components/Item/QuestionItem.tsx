483b877e702b8d1fa6d7a5288bc0438f05184f7c
winter100 <100taeng@gmail.com>
2024-03-11 22:45:24 +0900

    아이템 컴포넌트 추가

---
+++ src/components/Item/QuestionItem.tsx
@@ -0,0 +1,45 @@
+import { QuizItemPropsType } from "../../_type/type";
+import styles from "./QuestionItem.module.css";
+
+export default function QuestionItem({
+  question,
+  index,
+  dispatch,
+  answer,
+  questionLength,
+  score,
+}: QuizItemPropsType) {
+  const hasAnswered = answer !== null;
+  return (
+    <div className={styles.container}>
+      <div className={styles.score}>
+        <span>
+          점수: {score} / {5 * questionLength}
+        </span>
+        <span>
+          문제수: {index + 1} / {questionLength}
+        </span>
+      </div>
+      <div>
+        <h2>
+          <span className={styles.q_number}>{` [ ${index + 1} ]. `}</span>
+          <span className={styles.q_content}>{`${question.question}`}</span>
+        </h2>
+      </div>
+      <div className={styles.options}>
+        {question.options.map((item, i) => (
+          <button
+            disabled={hasAnswered}
+            key={item}
+            className={`${styles.option} ${i === answer ? styles.select : ""} ${
+              hasAnswered && i === question.answer ? styles.answer : ""
+            }`}
+            onClick={() => dispatch({ type: "ANSWER", payload: i })}
+          >
+            {item}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
