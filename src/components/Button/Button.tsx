c1c437d429ea9b8c2c7e092ed4d921afae43fc46
winter100 <100taeng@gmail.com>
2024-03-12 21:04:16 +0900

    버튼 컴포넌트 추가 및 css추가

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,19 @@
-import { QuizAction } from "../../_type/type";
 import styles from "./Button.module.css";
 
 export default function Button({
-  dispatch,
+  onClick,
   text,
+  bgColor = "#FFE5E5",
 }: {
-  dispatch: React.Dispatch<QuizAction>;
   text: string;
+  onClick: () => void;
+  bgColor?: string;
 }) {
   return (
     <button
+      style={{ backgroundColor: bgColor }}
       className={styles.button}
-      onClick={() => dispatch({ type: "NEXT" })}
+      onClick={onClick}
     >
       {text}
     </button>
